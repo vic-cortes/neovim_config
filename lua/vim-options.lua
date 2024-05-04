5f0e33e56712736661260ca7e05846bf7c7ed6e8
Victor Cortes Figueroa <vmcortesf@gmail.com>
2024-05-04 14:00:42 -0500

    Added bufferline

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,3 +3,8 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
+
+-- Bufferline
+vim.keymap.set("n", "<leader>n", ":bn<cr>")
+vim.keymap.set("n", "<leader>p", ":bp<cr>")
+vim.keymap.set("n", "<leader>x", ":bd<cr>")
