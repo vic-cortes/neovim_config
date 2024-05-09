b7a8202605941f3d358f088acc4f348904f1c8f5
Victor Cortes Figueroa <vmcortesf@gmail.com>
2024-05-09 09:29:29 -0500

    Added comments

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -8,3 +8,6 @@ vim.cmd("set number")
 vim.keymap.set("n", "<leader>n", ":bn<cr>")
 vim.keymap.set("n", "<leader>p", ":bp<cr>")
 vim.keymap.set("n", "<leader>x", ":bd<cr>")
+
+-- nvim-comment
+vim.keymap.set({"n", "v"}, "<leader>/", ":CommentToggle<cr>")
